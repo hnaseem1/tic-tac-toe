86388884757eb4f0d99f8c3111906ca5cc79e3ad
Hasan Naseem <hn92@live.com>
2018-03-21 18:34:13 -0400

    exercise11

---
+++ 03-week2/collections-iteration/exercise10.rb
@@ -0,0 +1,12 @@
+range = (1..100)
+range.each do |num|
+  if num%3 == 0
+    p "bit"
+  elsif num%5 == 0
+    p "Maker"
+  elsif num%3 == 0 && num%5 === 0
+    p "Bitmaker"
+  else
+    p num
+  end 
+end
