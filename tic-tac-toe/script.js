acb660fa6ca9e4a139f201fd1e229348073a31f2
Hasan Naseem <33922244+hnaseem1@users.noreply.github.com>
2018-05-17 15:16:08 -0400

    Update script.js

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,16 +4,50 @@ var x = 0
 var game = document.querySelector('.game')
 game.addEventListener('click', function(event) {
 
-    if (x === 0) {
+    if (x === 0  && event.srcElement.style.backgroundColor === '') {
       event.srcElement.style.backgroundColor = 'yellow';
-      score[x].push(event.srcElement.className);
+
+      checkWin(x, 'Yellow')
       x = 1
-    } else if (x === 1) {
+
+    } else if (x === 1 && event.srcElement.style.backgroundColor === '') {
       event.srcElement.style.backgroundColor = 'red';
-      score[x].push(event.srcElement.className);
+      checkWin(x, 'Red')
       x = 0
     }
-
-    console.log(score);
-
+    // console.log(score);
 });
+
+// Checks for one combination that was causing bug in the game
+
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
+}
+
+// check for the winning combinations using sums of all        possible outcomes
+function checkWin(x, color) {
+
+  var win = [6, 15, 18, 24]
+  score[x].push(Number(event.srcElement.id));
+
+  if (score[x].length === 3) {
+    var sum = score[x].reduce(function(acc, val) { return acc + val; });
+
+    for (var j = 0; j < win.length; j++) {
+      if (sum === win[j] || arraysEqual(score[x], [1, 4, 7])) {
+        document.querySelector('body').innerText = color + ' Wins!'
+        }
+      }
+
+    score[x].shift()
+
+    }
+
+}
